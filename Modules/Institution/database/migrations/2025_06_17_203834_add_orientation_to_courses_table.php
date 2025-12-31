f3797a462081a44e14dad1dc01fc9505a0260d81
Jeancy-Grys-Kalunga <jeancgryskalungadeveloper@gmail.com>
2025-12-31 17:02:49 +0200

    🐛 FIX: migrations

--- Modules/Institution/database/migrations/2025_06_17_203834_add_orientation_to_courses_table.php
+++ Modules/Institution/database/migrations/2025_06_17_203834_add_orientation_to_courses_table.php
@@ -9,10 +9,10 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-     public function up(): void
+    public function up(): void
     {
         Schema::table('courses', function (Blueprint $table) {
-            $table->string('orientation')->nullable()->after('credits');
+            $table->string('orientation')->nullable()->after('title');
         });
     }
 
