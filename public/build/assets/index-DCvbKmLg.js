8b7f9686351a268d1f4f77e2a0a4291c5c4928d5
Jeancy-Grys-Kalunga <jeancgryskalungadeveloper@gmail.com>
2026-06-03 17:48:01 +0200

    feat: replace svg logo with a new modern generated logo image

---
+++ public/build/assets/index-DCvbKmLg.js
@@ -0,0 +1,1 @@
+import{c as r,g as t}from"./app-DupQO1zg.js";var a=r();const e=t(a);export{e as R,a as r};
---
+++ public/build/assets/master-prediction-CSMHokzB.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-DupQO1zg.js";function i(){return r.jsx("div",{children:r.jsx("h1",{children:"Prédiction Master"})})}export{i as default};
