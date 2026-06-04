4d0e5b7cae93a465204db8b733502730c1a3c637
Jeancy-Grys-Kalunga <jeancgryskalungadeveloper@gmail.com>
2026-06-04 06:34:16 +0200

    Fix SimulateDeliberation: Add missing date_of_birth to student creation

--- app/Console/Commands/SimulateDeliberation.php
+++ app/Console/Commands/SimulateDeliberation.php
@@ -122,6 +122,7 @@ class SimulateDeliberation extends Command
                     'name' => 'Test Etudiant ' . ($i + 1),
                     'institution_id' => $institution->id,
                     'gendre' => 'M',
+                    'date_of_birth' => '2000-01-01',
                 ]);
                 $students->push($student);
             }
