0858d7df335fc9e6278942660267c3c20d8f31d3
Jeancy-Grys-Kalunga <jeancgryskalungadeveloper@gmail.com>
2026-06-04 09:07:30 +0200

    Fix button navigation with router.get

--- resources/js/pages/inscription/index.tsx
+++ resources/js/pages/inscription/index.tsx
@@ -459,7 +459,7 @@ export default function InscriptionIndex({ inscriptions: allInscriptions, can, f
                                 <Button
                                     variant="outline"
                                     className="gap-2 border-indigo-200 shadow-sm hover:bg-indigo-50"
-                                    onClick={() => (window.location.href = route('jury.prediction.interface'))}
+                                    onClick={() => router.get(route('jury.prediction.interface'))}
                                 >
                                     <BrainCircuit size={16} className="text-indigo-600" />
                                     Analyse Prédictive Master
